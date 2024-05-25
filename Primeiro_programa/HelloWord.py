6461cb764263cf47bdb1704e7beb058e78899a62
Clairtonlm <clairtonlm@gmail.com>
2024-05-25 00:17:16 -0300

    Função Print

--- Primeiro_programa/HelloWord.py
+++ Primeiro_programa/HelloWord.py
@@ -1,2 +1,11 @@
 # Projeto Python
 print("Hello Word")
+
+"""
+DocString
+Posso  escrever o que quiser
+
+ """
+'''
+Aspas simples pode ser usando também
+'''
---
+++ Primeiro_programa/aula2.py
@@ -0,0 +1,3 @@
+#Aula 02 - Função Print
+print(12, 24, sep='qualquer cosia')
+print("Clairton", "Lima", sep=".")
