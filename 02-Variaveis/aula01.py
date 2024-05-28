ec221344014cab627babb44c5d185c847265ab62
Clairtonlm <clairtonlm@gmail.com>
2024-05-27 23:06:26 -0300

    Iniciando Variaveis

---
+++ 02-Variaveis/aula01.py
@@ -0,0 +1,13 @@
+# Variále - PEP8 - codumento de guia de stilo do Python
+
+nome = 'ClairtonLima'
+sobrenome= "Lima"
+ano_nascimento = 1980
+maio_idade = 18
+altura_metros = 1.70
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", ano_nascimento)
+print("Maior de Idade:", maio_idade)
+print("Altura:", altura_metros)
