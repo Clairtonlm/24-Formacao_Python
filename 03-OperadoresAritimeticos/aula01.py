7032e00ed2eac40dfa2ab59373d436c3460b74e7
Clairtonlm <clairtonlm@gmail.com>
2024-05-28 22:02:02 -0300

    [28052024]-Py

---
+++ 03-OperadoresAritimeticos/aula01.py
@@ -0,0 +1,18 @@
+# Operadores Aritimeticos
+adcao = 10 + 10;
+print('Adição:', adcao)
+
+# ele corta os numeros depois do ponto nostra so o inteiro
+divisao_inteira = 10 // 3
+print(divisao_inteira)
+
+divisao = 10 / 3 # float
+print(divisao)
+
+# exponenciação
+exponenciacao = 2 ** 2
+print( 'expoente 2 elevado a 2: ', exponenciacao)
+
+#moludo --> mostra o resto da divisão
+modulo = 5 % 2
+print("Modulo", modulo)
