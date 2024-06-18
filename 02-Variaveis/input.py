ba37fb0e7e794c830d0b674bcc85b87a4d667a6d
Clairtonlm <Clairtonlm@gmail.com>
2024-06-17 22:18:31 -0300

    FunçãoInput em Py

---
+++ 02-Variaveis/input.py
@@ -0,0 +1,12 @@
+# Função Input
+
+nome = input('Qual seu nome?\n')
+print (f'O seu nome é {nome}')
+
+num_1 = input('Digite um número\n')
+num_2 = input('Digite um número\n')
+
+int_num_1 = int(num_1)
+int_num_2 = int(num_2)
+
+print(f'A soma dos números é igual a {int_num_1 + int_num_2}')
