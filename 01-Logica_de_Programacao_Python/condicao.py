f6fbefe055dd54abef2ebcc1f9154e2026308ae0
Clairtonlm <Clairtonlm@gmail.com>
2024-06-17 23:00:02 -0300

    comdecao if-elif-else

---
+++ 01-Logica_de_Programacao_Python/condicao.py
@@ -0,0 +1,12 @@
+# If/ elif/ else
+# se/ se não se / se não
+
+
+entrada = input('Você quer "entrar" ou "Sair"?\n')
+
+if entrada == 'entrar':
+    print('Voce entrou')
+elif entrada == 'sair':
+    print('Voce saiu do sistema')
+else:
+    print('Voce não digitou nem entrar e nem sair')
